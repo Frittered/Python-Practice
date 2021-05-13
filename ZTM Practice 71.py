4d43b1e46a62aac142c4aef534f8ddda49d4223d
Fritz Stevenson <76401499+Fritz-Stevenson@users.noreply.github.com>
2021-05-12 22:30:42 -0700

    more random.choice() list comprehension

--- ZTM Practice 71.py
+++ ZTM Practice 71.py
@@ -1,3 +1,4 @@
+# more random.choice() list comprehension 
 import random 
 def print_random_factor_of_5_and_7():
     return print(random.choice([x for x in range(10,150) if (not x%5 and not x%7)]))
