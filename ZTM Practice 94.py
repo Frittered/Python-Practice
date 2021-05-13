2819a8569de8bbec35ebf158ef48ec3b46c33cab
Fritz Stevenson <76401499+Fritz-Stevenson@users.noreply.github.com>
2021-05-12 22:47:20 -0700

    Multi-variable algebra

--- ZTM Practice 94.py
+++ ZTM Practice 94.py
@@ -1,6 +1,5 @@
 # Write a program to solve a classic ancient Chinese puzzle: We count 35 heads and 94 legs among the chickens and rabbits in a farm. 
 # How many rabbits and how many chickens do we have?
-
 from sympy import Eq, solve
 from sympy.abc import x, y
 eq1 = Eq((x*2 + y*4), 94)
