c0b6dac2d70ece472b1c210e37eca7c6ef027f53
Fritz Stevenson <76401499+Fritz-Stevenson@users.noreply.github.com>
2021-05-13 22:12:32 -0700

    Create function with if else user input

--- ZTM Practice 40.py
+++ ZTM Practice 40.py
@@ -1,3 +1,4 @@
+# Create function with if else user input
 def accept_yes(user_input):
     return print('Yes') if user_input.upper() == 'YES' else print('No')
 accept_yes(input('Can you type yes?  '))
