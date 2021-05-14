8b16c559645a357b9fb6132d52e46db1b0294860
Fritz Stevenson <76401499+Fritz-Stevenson@users.noreply.github.com>
2021-05-13 22:35:57 -0700

    Print the longer of two strings

--- ZTM Practice 30.py
+++ ZTM Practice 30.py
@@ -1,3 +1,4 @@
+# Print the longer of two strings
 which_string_is_longer = lambda a, b: print(a) if len(a)>len(b) else (print(b) if len(b)>len(a) else print(f'{a} and {b} are both {len(a)} long'))
 which_string_is_longer(input('a:  '), input('b:  '))
     
