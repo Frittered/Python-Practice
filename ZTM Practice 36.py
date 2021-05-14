4be10b84ac6922296791c9531227f494d0baaca9
Fritz Stevenson <76401499+Fritz-Stevenson@users.noreply.github.com>
2021-05-13 22:25:33 -0700

    print square values except 5

--- ZTM Practice 36.py
+++ ZTM Practice 36.py
@@ -1,4 +1,5 @@
+#print square values except 5
 def print_sq_values_except_5():
     sq_list = [[i,i**2] for i in range(1,21)]
-    print(sq_list[5:])
+    print(sq_list[:4],sq_list[5:])
 print_sq_values_except_5()
